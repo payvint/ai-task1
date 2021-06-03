6bcdfc092929a2b607f21dcc358418090164ae7f
Artem Payvin <payvinartem@gmail.com>
2021-06-03 03:10:23 +0300

    Add static_root

--- cosmetics/settings.py
+++ cosmetics/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 django_heroku.settings(locals())
 
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
