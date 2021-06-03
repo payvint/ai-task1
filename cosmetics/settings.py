1718e76069c0521f1123c7f7fd3536e03e2685ba
Artem Payvin <payvinartem@gmail.com>
2021-06-03 03:17:36 +0300

    rm unused vars

--- cosmetics/settings.py
+++ cosmetics/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-import os
 
 django_heroku.settings(locals())
 
@@ -123,8 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-DISABLE_COLLECTSTATIC = 1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
